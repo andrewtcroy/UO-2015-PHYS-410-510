3a134c434644ebe4b0d1667c79aec749b4cb6818
andrewtcroy <andrewtcroy@gmail.com>
2015-05-28 23:23:28 -0700

    3

--- week_9/hw7/smoothing.py
+++ week_9/hw7/smoothing.py
@@ -10,6 +10,12 @@ value = [i[1] for i in data]
 #for i in data: 
 #   time += [i[0]]
 
-graph, = plt.plot(time, value)
-plt.show()
+#graph, = plt.plot(time, value)
+#plt.title("Unsmoothed Raw Data")
 
+bxsmoothdata = [np.mean(value[i:i+5]) for i in range(len(data)-5)]
+time = [i for i in range(len(data)-5)]
+
+graph2 = plt.plot(time, bxsmoothdata)
+plt.title("BOXsmoothed by 5 Data")
+plt.show()
