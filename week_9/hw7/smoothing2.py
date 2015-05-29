53106e43248a1c781e11314378f75a207f8e488e
andrewtcroy <andrewtcroy@gmail.com>
2015-05-29 01:38:43 -0700

    5

--- week_9/hw7/smoothing2.py
+++ week_9/hw7/smoothing2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from linearplus import linearplus as lp
 import random
 import math
-from _future_ import division
+
 data = np.genfromtxt("data.csv", delimiter=',')
 time = [i[0] for i in data]
 value = [i[1] for i in data]
@@ -16,21 +16,29 @@ value = [i[1] for i in data]
 bxsmoothdata = [np.mean(value[i:i+5]) for i in range(len(data)-5)]
 time = [i+5 for i in range(len(data)-5)]
 
-chisquared = 0
+models = [[]]
 
-for t in time:
+for i in range(100):
 	m = random.uniform(1, 10)
 	a = random.uniform(1, 10)
 	b = random.uniform(1, 10)
 	w = random.uniform(1, 10)
 	phi = random.uniform(1, 10)
 
-	model = lp(t,m,a,b,w,phi)
+	models += [0,m,b,a,w,phi]
+	
+	for t in time:
+		model = lp(t,m,a,b,w,phi)
+		print(models)
+		models[i][0] += (math.pow((bxsmoothdata[t] - model),2) / model)
 
-chisquared = math.pow((value[t] - model[t]),2) / model[t]
+bestfitval = 1000
+bestfitmodel = []
 
-
-
+for model in models:
+	if model[0] < bestfitval:
+		bestfitval = model[0]
+		bestfitmodel = model
 
 graph2 = plt.plot(time, bxsmoothdata)
 plt.title("BOXsmoothed by 5 Data")
